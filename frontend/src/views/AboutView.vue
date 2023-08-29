4a050477a4268b1d6147ed5b2663ac263d1fd06b
ganiefah <ganiefahmajiet3@gmail.com>
2023-08-29 15:33:24 +0200

    added about

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
+  <div class="body">
+    <div class="container">
+      <div class="about">
+        <h1>About Sonic Vinyls: Where Music Finds Its Groove</h1>
+        <h3>Welcome to Sonic Vinyls, your ultimate destination for embracing the timeless allure of music through the rich, warm embrace of vinyl records. We are more than just a vinyl store – we are curators of auditory nostalgia, keepers of sonic stories, and champions of the analog music experience.</h3>
+      </div>
+      <br>
+      <div class="info">
+        <h1>Our Story</h1>
+        <h3>At Sonic Vinyls, our journey is a harmonious blend of passion for music and a deep appreciation for the tangible art of vinyl records. It all began with a group of avid music enthusiasts who shared a common dream: to bring back the magic of vinyl and make it accessible to the modern world. This dream evolved into the vibrant reality that is Sonic Vinyls today.</h3>
+      </div>
+      <br>
+      <div class="img">
+        <img src="https://i.postimg.cc/htGSsfFT/Screenshot-2023-08-29-141741.png" alt="">
+      </div>
+    </div>
+    <br>
+    <div class="team">
+      <h1>Meet Our Team</h1>
+
+    </div>
 
   </div>
 </template>
@@ -11,5 +31,12 @@
 </script>
 
 <style scoped>
+img{
+  height: 50vh;
+  width: 100%;
+}
 
+.body{
+height: 200vh;
+}
 </style>
