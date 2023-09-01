1404f0c2029f2f5892513b7bf79a73f0e2455e6d
ganiefah <ganiefahmajiet3@gmail.com>
2023-09-01 10:59:10 +0200

    added to my about and to my contact

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,6 +1,148 @@
 <template>
     <div>
-
+        <h1>Users Table</h1>
+        <AddUser />
+        <center>
+          <div class="table-responsive">
+        <table class="table table-bordered border-black text-center" >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>Role</th>
+              <th>Email Address</th>
+              <th>Profile Image</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody v-for="user in users" :key="user">
+            <tr v-if="user">
+              <td>{{ user.firstName }}</td>
+              <td>{{ user.lastName }}</td>
+              <td>{{ user.userAge }}</td>
+              <td>{{ user.Gender }}</td>
+              <td>{{ user.userRole }}</td>
+              <td>{{ user.emailAdd }}</td>
+              <td>
+                <img
+                  :src="user.userProfile"
+                  :alt="user.firstName"
+                  loading="lazy"
+                  class="img-fluid image"
+                />
+              </td>
+              <td>
+              <UpdateUser :user="user"/>  
+                <button class="del" @click="deleteUser(user.UserID)">delete</button>
+              </td>
+            </tr>
+            <tr v-else>
+              <Spinner />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </center>
+      <br>
+    
+        <h1>Products Table</h1>
+        <AddProducts />
+        <center>
+          <div class="table-responsive">
+          <table class="table table-bordered border-black text-center">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>name</th>
+              <th>quantity</th>
+              <th>price</th>
+              <th>category</th>
+              <th>product image</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody v-for="product in filteredProducts" :key="product.prodID">
+            <tr v-if="product">
+              <td>{{ product.prodID }}</td>
+              <td>{{ product.prodName }}</td>
+              <td>{{ product.quantity }}</td>
+              <td>R {{ product.amount }}</td>
+              <td>{{ product.Category }}</td>
+              <td>
+                <img
+                  :src="product.prodUrl"
+                  :alt="product.prodName"
+                  class="img-fluid image"
+                  loading="lazy"
+                />
+              </td>
+              <td>
+                <UpdateProducts :product="product"/>
+                <button class="del" @click="remveProduct(product.prodID)">
+                  delete
+                </button>
+              </td>
+            </tr>
+            <tr v-else>
+              <Spinner />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </center>
+    
+        <h1>Orders Table</h1>
+        <AddOrders/>
+        <center>
+          <div class="table-responsive">
+          <table class="table table-bordered border-black text-center">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User's ID</th>
+              <th>Product ID</th>
+              <th>barcode</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>OrderDate</th>
+              <th>Picture</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody v-for="order in orders" :key="order.orderID">
+            <tr v-if="Orders">
+              <td>{{ order.orderID }}</td>
+              <td>{{ user.userID }}</td>
+              <td>{{ product.prodID }}</td>
+              <td>{{ order.barcode }}</td>
+              <td>{{ order.productName }}</td>
+              <td>{{ order.price }}</td>
+              <td>{{ order.OrderDate }}</td>
+              <td>{{ order.picture }}</td>
+              <td>
+                <img
+                  :src="product.prodUrl"
+                  :alt="product.prodName"
+                  class="img-fluid image"
+                  loading="lazy"
+                />
+              </td>
+              <td>
+                <UpdateProducts :product="product"/>
+                <button class="del" @click="remveProduct(product.prodID)">
+                  delete
+                </button>
+              </td>
+            </tr>
+            <tr v-else>
+              <Spinner />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </center>
     </div>
 </template>
 
--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -1,7 +1,50 @@
 <template>
     <div>
-
+        <div class="info">
+       <h1> Welcome to the Sonic Vinyls Help Center</h1>
+       <h4> At Sonic Vinyls, we're passionate about all things vinyl records, and we're committed to making your vinyl shopping experience as smooth as the grooves on your favorite vinyl. Our Help Center is here to assist you with any questions or concerns you may have while exploring our store and enjoying your vinyl journey.</h4>
     </div>
+    <center>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Accordion Item #1
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+    </center>
+</div>
 </template>
 
 <script>
@@ -11,5 +54,15 @@
 </script>
 
 <style scoped>
+h4{
+    margin-left: 1rem;
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+}
 
+.accordion{
+    width: 75%;
+    
+    
+}
 </style>
